f55177cbaf783b6547f11e40f07dfa936b4cfdfe
jrathod9 <lewisburnard@gmail.com>
2019-12-21 15:30:52 +0530

    App setup complete

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template, request, session
-from flask_session import Session
+from flask import Flask, render_template, request
+import os
 
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
